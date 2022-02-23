7b74ffafb3bbd42010cd52f206e8fc6573da1732
Javier Nieto <99614201+Javier-Nieto23@users.noreply.github.com>
2022-02-23 13:11:43 -0800

    agree the body of the page and the archive style.css

--- Index.php
+++ Index.php
@@ -3,7 +3,37 @@
 <!DOCTYPE html>
     <head>
         <!--this is the title of the page-->
-        <meta http-equiv="Contact type" content="text/html; charset= utf-8" / >
+        <meta http-equiv="Contact type" content="text/html; charset= utf-8"/>
+        <link rel = "stylesheet" type = "text/css" href = ""/>
+        <!-- this line code is for agree a icon in the webpage -->
+        <link rel = "icon" type = "" href = ""/>
+        <center><title>Electronics</title></center>
+
+        <!--body of the web page-->
+        <body>
+        <center><p>Electronics </p>
+        <!-- this a submenu of the page-->
+        <div id = "menu wrapper">
+            <li><a href = "Login.php">Login</a></li>
+            <li><a href = "">home</a></li>
+            <li><a href = "">Products</a>
+            <!-- this a submenu for the web page-->
+            <ul class = "sub-menu">
+                <li><a href = "">All</a></li>  
+                <li><a href = "">Celphone</a></li>
+                <li><a href = "">Laptops</a></li>
+                <li><a href = "">Gamers</a></li>
+            </ul>
+            <!--this a submenu for the networks-->
+            <li><a href = "">Social networks</a></li>
+            <ul class= "sub-menu">
+                <li><a href="#" category = "#">Facebook</a></li>
+                <li><a href="#" category = "#">Twitter</a></li>
+                <li><a href="#" category = "#">GitHub</a></li>
+            </ul>
+        
+        </body>
+
 
     </head>
 

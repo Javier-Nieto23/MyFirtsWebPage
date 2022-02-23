84dd4b8eee4222668a7ccd3bccae8bc8e3cf3538
Javier Nieto <99614201+Javier-Nieto23@users.noreply.github.com>
2022-02-23 02:15:25 -0800

    agree the line code <head> for the tittle

--- Index.php
+++ Index.php
@@ -1,2 +1,14 @@
 <!-- this is my Firts Web Page-->
 
+<!DOCTYPE html>
+    <head>
+        <!--this is the title of the page-->
+        <meta http-equiv="Contact type" content="text/html; charset= utf-8" / >
+
+    </head>
+
+</html>
+
+
+
+
